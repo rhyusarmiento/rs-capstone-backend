ddab8bf29be7d9b21e9b7ef804491b80713c86eb
Rhyu Sarmiento <rhyu.sar.15@gmail.com>
2021-03-23 14:07:33 -0600

    FIX table and player team relationship

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Dependency Docs
 - [Flask](https://flask.palletsprojects.com/en/1.1.x/)
 - [Flask SQLAlchemy](https://flask-sqlalchemy.palletsprojects.com/en/2.x/)
+- [SQLAlchemy](https://www.sqlalchemy.org/)
 - [Flask Marshmallow](https://flask-marshmallow.readthedocs.io/en/latest/)
 - [Marshmallow-sqlalchemy](https://marshmallow-sqlalchemy.readthedocs.io/en/latest/)
 - [flask-cors](https://flask-cors.readthedocs.io/en/latest/)
--- app.py
+++ app.py
@@ -11,9 +11,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-player_teams = db.Table('player_teams',
-    db.Column('player_id', db.Integer, db.ForeignKey('player.id'), primary_key=True),
-    db.Column('team_id', db.Integer, db.ForeignKey('team.id'), primary_key=True)
+players_teams = db.Table('players_teams',
+    db.Column(
+        'player_id', 
+        db.Integer, 
+        db.ForeignKey('player.id')
+    ),
+    db.Column(
+        'team_id', 
+        db.Integer, 
+        db.ForeignKey('team.id')
+    )
 )
 
 class Player(db.Model):
@@ -24,47 +32,54 @@ class Player(db.Model):
     phone_number = db.Column(db.String(13), unique=False, nullable=False)
     teams = db.relationship(
         'Team',
-        secondary=player_teams, 
+        secondary=players_teams, 
         lazy='subquery', 
-        backref=db.backref('player', lazy=True),
+        backref=db.backref('teams', lazy=True),
     )
 
 class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=False)
-    city = db.Column(db.String(20), unique=False)
-    state = db.Column(db.String(2), unique=False)
-    sport = db.Column(db.String(15), unique=False)
-    matchup = db.relationship('Matchup', backref='team', lazy=True)
+    name = db.Column(db.String(20), unique=False, nullable=False)
+    city = db.Column(db.String(20), unique=False, nullable=False)
+    state = db.Column(db.String(2), unique=False, nullable=False)
+    sport = db.Column(db.String(15), unique=False, nullable=False)
+    players = db.relationship(
+        'Player',
+        secondary=players_teams, 
+        lazy='subquery', 
+        backref=db.backref('players', lazy=True)
+    )
 
-class Matchup(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    when = db.Column(db.String(30), unique=False)
-    where = db.Column(db.String(20), unique=False)
-    team1 = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
-    team2 = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
-    active = db.Column(db.Boolean, unique=False)
+# class Matchup(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     when = db.Column(db.String(30), unique=False, nullable=False)
+#     where = db.Column(db.String(20), unique=False, nullable=False)
+#     team1_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+#     team2_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+#     active = db.Column(db.Boolean, unique=False, nullable=False)
+#     team1 = db.relationship('Matchup', foreign_keys=[team1_id])
+#     team2 = db.relationship('Matchup', foreign_keys=[team2_id])
 
 class PlayerSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'city', 'state', 'phone_number', 'teams')
+        fields = ('id', 'name', 'city', 'state', 'phone_number')
 
 player_schema = PlayerSchema()
 players_schema = PlayerSchema(many=True)
 
 class TeamSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'city', 'state', 'sport', 'matchup')
+        fields = ('id', 'name', 'city', 'state', 'sport')
 
 team_schema = TeamSchema()
 teams_schema = TeamSchema(many=True)
 
-class MatchupSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'name', 'city', 'state', 'sport', 'matchup')
+# class MatchupSchema(ma.Schema):
+#     class Meta:
+#         fields = ('id', 'when', 'where', 'team1_id', 'team2_id', 'active')
 
-matchup_schema = MatchupSchema()
-matchups_schema = MatchupSchema(many=True)
+# matchup_schema = MatchupSchema()
+# matchups_schema = MatchupSchema(many=True)
 
 @app.route('/api/add-player', methods=['POST'])
 def add_player():
@@ -73,7 +88,6 @@ def add_player():
     city = post_data.get('city')
     state = post_data.get('state')
     phone_number = post_data.get('phone_number')
-    # teams = post_data.get('teams')
     new_player = Player(name=name, city=city, state=state, phone_number=phone_number)
     db.session.add(new_player)
     db.session.commit()
@@ -84,18 +98,23 @@ def get_players():
     all_players = Player.query.all()
     return jsonify(players_schema.dump(all_players))
 
-# @app.route('/api/add-player', methods=['POST'])
-# def add_player():
-#     post_data = request.get_json()
-#     name = post_data.get('name')
-#     city = post_data.get('city')
-#     state = post_data.get('state')
-#     phone_number = post_data.get('phone_number')
-#     # teams = post_data.get('teams')
-#     new_player = Player(name=name, city=city, state=state, phone_number=phone_number)
-#     db.session.add(new_player)
-#     db.session.commit()
-#     return jsonify(player_schema.dump(new_player))
+@app.route('/api/add-team', methods=['POST'])
+def add_team():
+    post_data = request.get_json()
+    name = post_data.get('name')
+    city = post_data.get('city')
+    state = post_data.get('state')
+    sport = post_data.get('sport')
+    # teams = post_data.get('teams')
+    new_team = Team(name=name, city=city, state=state, sport=sport)
+    db.session.add(new_team)
+    db.session.commit()
+    return jsonify(player_schema.dump(new_team))
+
+@app.route('/api/get-teams')
+def get_teams():
+    all_teams = Team.query.all()
+    return jsonify(teams_schema.dump(all_teams))
 
 if __name__ == '__main__':
     app.run(debug=True)
